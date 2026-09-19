770e228a75b481342ed48b20c9336a5596e52b8f
Hobarrt <hoobarrt@gmail.com>
2026-09-19 13:51:47 +0800

    Upload PT 2

--- posttest/posttest-apd-2/2609106019-m.rasyid_ridha-pt-2.py
+++ posttest/posttest-apd-2/2609106019-m.rasyid_ridha-pt-2.py
@@ -4,17 +4,17 @@ bagasi_3 = 7
 bagasi_4 = 15
 bagasi_5 = 20
 bagasi_6 = 10
+nim = 19
 
 bagasi = [bagasi_1, bagasi_2, bagasi_3, bagasi_4, bagasi_5, bagasi_6]
 total_berat = bagasi_1 + bagasi_2 + bagasi_3 + bagasi_4 + bagasi_5 + bagasi_6
-total_berat_akhir = total_berat + (total_berat * 5 / 100)
-rata_rata = total_berat_akhir / len(bagasi)
+total_bayar = total_berat + (total_berat * 5 / 100)
+rata_rata = total_berat / len(bagasi)
 
-nim = 19
 bolean = nim < rata_rata
 
 pemumpang_tengah = bagasi[2:5]
-total_berat_gram = total_berat_akhir * 1000
+total_berat_gram = total_berat * 1000
 
 print("berat bagasi 1 =", bagasi_1)
 print("berat bagasi 2 =", bagasi_2)
@@ -23,9 +23,9 @@ print("berat bagasi 4 =", bagasi_4)
 print("berat bagasi 5 =", bagasi_5)
 print("berat bagasi 6 =", bagasi_6)
 print("total berat bagasi =", total_berat)
-print("total berat setelah mempertimbangkan biaya kompensasi =", total_berat_akhir)
-print("rata-rata berat setiap penumpang =", rata_rata)
+print("total bayar setelah mempertimbangkan biaya kompensasi =", total_bayar)
+print("rata-rata berat setiap =", rata_rata)
 print("nim =", nim)
-print("apakah nim lebih kecil dari rata-rata berat =", bolean)
+print("bolean =", bolean)
 print("Berat bagasi penumpang tengah =", pemumpang_tengah)
 print("total berat dalam gram =", total_berat_gram)
